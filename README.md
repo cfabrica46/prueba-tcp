9f9f947d387e7da79b3f29aa4fb1451c7ca0b5ac
Arturo Nava <arthurnavah@gmail.com>
2021-01-18 18:35:14 -0500

    server

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Prueba basica del protocolo TCP con Golang.
 
 Se desarrollara un servidor TCP basico el cual podremos conectarnos usando un cliente TCP... por el momento utilizaremos netcat hasta desarrollar el cliente en Go.
+
+## Run Server
+
+`go build && ./prueba-tcp localhost 8080`
+
+o
+
+`go run main.go localhost 8080`
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) >= 2 {
+		serverAddress := fmt.Sprintf("%s:%s", os.Args[1], os.Args[2])
+
+		l, err := net.Listen("tcp", serverAddress)
+		if err != nil {
+			fmt.Println("Error listening:", err.Error())
+			os.Exit(1)
+		}
+		defer l.Close()
+
+		fmt.Println("Listening on " + serverAddress)
+		fmt.Println()
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			content, err := ioutil.ReadAll(conn)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println("Message received:", string(content))
+
+		}
+
+	}
+}
